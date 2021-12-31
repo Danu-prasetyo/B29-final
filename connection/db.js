96f3418f553f2d085710082e0cfa01c5a390ce09
danugans <mas.danu29@gmail.com>
2021-12-31 13:40:23 +0700

    fix database connection

--- connection/db.js
+++ connection/db.js
@@ -2,12 +2,10 @@ const { Pool } = require("pg");
 
 const dbPool = new Pool({
   //object
-  host: "ec2-54-161-238-249.compute-1.amazonaws.com",
-  database: "dvnbgmk0idi2h",
+  database: "personal_web",
   port: "5432",
-  user: "suyrfirkiuerik",
-  password: "9cc2a2917be55d37c561c2ad999d4f63be82aac3655fccf1625b95fb927a183a",
-  dialect: "postgres",
+  user: "postgres",
+  password: "root",
 });
 
 module.exports = dbPool;
